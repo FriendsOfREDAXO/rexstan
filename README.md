77a66454513dfa95efc5f8c106381390e2a0935e
Markus Staab <maggus.staab@googlemail.com>
2022-06-11 18:04:09 +0200

    wip

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 rexstan
 =======
+
+rexstan integrates [PHPStan](https://phpstan.org/) into REDAXO.
+
+## 💌 Support rexstan
+
+[Consider supporting the project](https://github.com/sponsors/staabm), so we can make this tool even better even faster for everyone.
+
--- pages/analysis.php
+++ pages/analysis.php
@@ -23,6 +23,13 @@ if (!is_array($phpstanResult) || !is_array($phpstanResult['files'])) {
     $basePath = rex_path::src('addons/');
 
     foreach ($phpstanResult['files'] as $file => $fileResult) {
+        $shortFile = str_replace($basePath, '', $file);
+
+        echo '<tr class="rexstan-error-file">';
+        echo '<td></td>';
+        echo '<td colspan="2"><span>'.rex_escape(dirname($shortFile)).DIRECTORY_SEPARATOR.'</span>'.rex_escape(basename($shortFile)).'</td>';
+        echo '</tr>';
+
         foreach ($fileResult['messages'] as $message) {
             $error = rex_escape($message['message']);
 
@@ -31,12 +38,12 @@ if (!is_array($phpstanResult) || !is_array($phpstanResult['files'])) {
                 $error = '<a href="'. $url .'">'. rex_escape($error) .'</a>';
             }
 
-            $shortFile = str_replace($basePath, '', $file);
 
-            echo '<tr>';
+
+            echo '<tr class="rexstan-error">';
             echo '<td></td>';
             echo '<td>'.$error.'</td>';
-            echo '<td><span>'.rex_escape(dirname($shortFile)).DIRECTORY_SEPARATOR.'</span>'.rex_escape(basename($shortFile)).':'.$message['line'] .'</td>';
+            echo '<td>'. rex_escape(basename($shortFile)).':'.$message['line'] .'</td>';
             echo '</tr>';
         }
     }
