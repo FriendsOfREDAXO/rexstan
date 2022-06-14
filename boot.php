314d8f6b6b3b4edf088f3b61a5fc1a5905f12b57
Markus Staab <maggus.staab@googlemail.com>
2022-06-14 20:57:49 +0200

    added level 9 achievement

--- boot.php
+++ boot.php
@@ -1,5 +1,6 @@
 <?php
 
-if (rex::isBackend() && is_object(rex::getUser())) {
+if (rex::isBackend() && is_object(rex::getUser()) && 'rexstan' === rex_be_controller::getCurrentPagePart(1) ) {
     rex_view::addCssFile($this->getAssetsUrl('rexstan.css'));
+    rex_view::addJsFile($this->getAssetsUrl('confetti.min.js'));
 }
--- pages/analysis.php
+++ pages/analysis.php
@@ -52,9 +52,33 @@ if (!is_array($phpstanResult) || !is_array($phpstanResult['files'])) {
         }
 
         echo '<span class="rexstan-achievement">'.$emoji .'</span>';
-        echo rex_view::success('Gratulation, es wurden keine Fehler gefunden.');
+        echo rex_view::success('Gratulation, es wurden keine Fehler in Level '. $level .' gefunden.');
 
-        if ($level !== 9) {
+        if ($level === 9) {
+            echo '<script>
+                var duration = 10 * 1000;
+                var animationEnd = Date.now() + duration;
+                var defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
+
+                function randomInRange(min, max) {
+                  return Math.random() * (max - min) + min;
+                }
+
+                var interval = setInterval(function() {
+                  var timeLeft = animationEnd - Date.now();
+
+                  if (timeLeft <= 0) {
+                    return clearInterval(interval);
+                  }
+
+                  var particleCount = 50 * (timeLeft / duration);
+                  // since particles fall down, start a bit higher than random
+                  confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 } }));
+                  confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 } }));
+                }, 250);
+                </script>
+            ';
+        } else {
             echo '<p>In den <a href="'. rex_url::backendPage('rexstan/settings') .'">Einstellungen</a>, solltest du jetzt das nächste Level anvisieren.</p>';
         }
         return;
