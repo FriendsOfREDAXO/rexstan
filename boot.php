31374f210c49783dce1b2a70aff13af10e6ca084
Andreas Eberhard <andreaseberhard@gmail.com>
2022-06-29 00:38:11 +0200

    Update boot.php

--- boot.php
+++ boot.php
@@ -1,6 +1,7 @@
 <?php
+$addon = rex_addon::get('rexstan');
 
 if (rex::isBackend() && is_object(rex::getUser()) && 'rexstan' === rex_be_controller::getCurrentPagePart(1) ) {
-    rex_view::addCssFile($this->getAssetsUrl('rexstan.css'));
-    rex_view::addJsFile($this->getAssetsUrl('confetti.min.js'));
+    rex_view::addCssFile($addon->getAssetsUrl('rexstan.css'));
+    rex_view::addJsFile($addon->getAssetsUrl('confetti.min.js'));
 }
