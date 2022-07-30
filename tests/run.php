4f229f6222279e2dfa02b50d94602d62b234edc3
Markus Staab <maggus.staab@googlemail.com>
2022-07-30 11:57:46 +0200

    simple unit tests

---
+++ tests/run.php
@@ -0,0 +1,22 @@
+<?php
+
+chdir(__DIR__.'/../');
+
+$output = trim((string) shell_exec('php vendor/bin/phpstan analyze tests/* --error-format=raw'));
+$output = preg_replace('/\s*$/', '', $output);
+$expected = trim((string) file_get_contents(__DIR__.'/expected.out'));
+
+if ($output != $expected) {
+    echo "ERROR, output does not match\n\n";
+
+    echo "OUTPUT:\n";
+    var_dump($output);
+
+    echo "EXPECTED:\n";
+    var_dump($expected);
+
+    exit(1);
+}
+
+echo "all good\n";
+exit(0);
