44a5801c41ec2702589244078a260c6c332048c2
Markus Staab <maggus.staab@googlemail.com>
2022-07-30 11:58:34 +0200

    simple unit tests (#56)

--- tests/data/sql-get-value.php
+++ tests/data/sql-get-value.php
@@ -12,7 +12,7 @@ function preparedQuery(int $id):void
     $sql = rex_sql::factory();
     $sql->setQuery('
             SELECT  name
-            FROM    ' . rex::getTable('cronjob') . '
+            FROM    ' . rex::getTable('article') . '
             WHERE   id = ?
             LIMIT   1
         ', [$id]);
@@ -25,11 +25,24 @@ function regularQuery():void
     $sql = rex_sql::factory();
     $sql->setQuery('
             SELECT  name
-            FROM    ' . rex::getTable('cronjob') . '
+            FROM    ' . rex::getTable('article') . '
             WHERE   id = 1
             LIMIT   1
         ');
 
     assertType('string', $sql->getValue('name'));
+}
+
+function unknownValue():void
+{
+    $sql = rex_sql::factory();
+    $sql->setQuery('
+            SELECT  name
+            FROM    ' . rex::getTable('article') . '
+            WHERE   id = 1
+            LIMIT   1
+        ');
+
+    assertType('bool|float|int|string|null', $sql->getValue('doesNotExist'));
 }
 
