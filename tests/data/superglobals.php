8bad86587fc7949ce3b09a9646fcbf42eaa52523
Markus Staab <maggus.staab@googlemail.com>
2022-08-04 13:32:05 +0200

    forbid all superglobals (#73)

--- tests/data/superglobals.php
+++ tests/data/superglobals.php
@@ -2,4 +2,11 @@
 
 function() {
     $_SESSION['foo'] = 'bar';
+    $x = $_GET['foo'];
+    $x = $_POST['foo'];
+    $x = $_REQUEST['foo'];
+    $x = $_COOKIE['foo'];
+    $x = $_SERVER['foo'];
+    $x = $_ENV['foo'];
+    $x = $_FILES['foo'];
 };
