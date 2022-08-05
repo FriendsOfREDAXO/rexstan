ecb33d89b99d2662e5e6056de4315c2093a80d5e
Markus Staab <maggus.staab@googlemail.com>
2022-08-05 09:48:23 +0200

    fix

--- lib/RexStanPhpCli.php
+++ lib/RexStanPhpCli.php
@@ -10,7 +10,7 @@ final class RexStanPhpCli {
             return $cliPhpVerssion;
         }
 
-        $latestVersionFolderName = RexStan::execCmd('command ls /Applications/MAMP/bin/php/ | sort -n | tail -1');
+        $latestVersionFolderName = RexStan::execCmd('command ls /Applications/MAMP/bin/php/ | sort -n | tail -1', $lastError);
         $mampPhpPath = '/Applications/MAMP/bin/php/'. $latestVersionFolderName .'/bin/php';
         if (is_file($mampPhpPath)) {
             $cliPhpVerssion = RexStan::execCmd($mampPhpPath.' -r "echo PHP_VERSION_ID;"', $lastError);
