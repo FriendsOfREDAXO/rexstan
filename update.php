8dc3876deec36a8ab27c8841250bea933fa285ea
Markus Staab <maggus.staab@googlemail.com>
2022-08-11 11:18:44 +0200

    Revert "remove auto-generated file which used to be exist in previous versions"
    This reverts commit f45b604a0d61064266c20a6797f5d315c7520fd7.

--- update.php
+++ update.php
@@ -3,6 +3,3 @@
 // trigger the installation procedures also on updates
 $addon = rex_addon::get('rexstan');
 $addon->includeFile(__DIR__ . '/install.php');
-
-// remove auto-generated file which used to be exist in previous versions
-rex_file::delete(__DIR__.'/phpstan.neon');
