c7a6082bb22e8a943184ed322e47dec11ab6a2f0
christophboecker <christoph_boecker@t-online.de>
2022-08-13 16:56:12 +0200

    nutzt Formular-Klasse RexStanSettings
    - Das Settings-Formular wird nun mit `RexStanSettings extends rex_config_form` erzeugt und verwaltet. Die neue Klasse unterstützt die .neon-Dateien im Addon. 
    - Der Aufbau des Formulars ist jetzt in `RexStanSettings::init()` verschoben. So muss bei weiteren Änderungen der Formularfelder nur eine Datei angefasst werden; `settings.php`kann bleiben wie sie ist.
    - `settings.php` ist reduziert auf
      - Formular-Instanz anlegen
      - Section-Fragment aufbauen
      - Formular via Fragment anzeigen

--- pages/settings.php
+++ pages/settings.php
@@ -1,61 +1,21 @@
 <?php
+/**
+ * Konfigurationsseite des Addons / Settings.
+ *
+ * Das Formular wird mit der Form-Klasse RexStanSettings, basierend auf rex_config_form, aufsetzt.
+ * Die Parametriesierung und der Formularaufbaue (Felder) ist komplett in RexStanSettings.php zu finden
+*/
 
-$form = rex_config_form::factory('rexstan');
+$form = redaxo\phpstan\RexStanSettings::factory('rexstan');
+$faqUrl = rex_url::backendPage('rexstan/faq');
 
-$field = $form->addInputField('number', 'level', RexStanUserConfig::getLevel(), ['class' => 'form-control', 'min' => 0, 'max' => 9]);
-$field->setLabel('Level');
-$field->setNotice('0 is the loosest and 9 is the strictest - <a href="https://phpstan.org/user-guide/rule-levels">see PHPStan Rule Levels</a>');
-
-$field = $form->addSelectField('addons', RexStanUserConfig::getPaths(), ['class' => 'form-control selectpicker', 'data-live-search' => 'true']); // die Klasse selectpicker aktiviert den Selectpicker von Bootstrap
-$field->setAttribute('multiple', 'multiple');
-$field->setLabel('AddOns');
-$field->setNotice('AddOns die untersucht werden sollen');
-$select = $field->getSelect();
-
-foreach (rex_addon::getAvailableAddons() as $availableAddon) {
-    $select->addOption($availableAddon->getName(), $availableAddon->getPath());
-
-    if ('developer' === $availableAddon->getName() && class_exists(rex_developer_manager::class)) {
-        $select->addOption('developer: modules', rex_developer_manager::getBasePath() .'/modules/');
-        $select->addOption('developer: templates', rex_developer_manager::getBasePath() .'/templates/');
-    }
-}
-
-$field = $form->addSelectField('extensions', RexStanUserConfig::getIncludes(), ['class' => 'form-control selectpicker']);
-$field->setAttribute('multiple', 'multiple');
-$field->setLabel('PHPStan Extensions');
-$field->setNotice('Weiterlesen bzgl. der verf&uuml;gbaren Extensions: <a href="https://phpstan.org/blog/what-is-bleeding-edge">Bleeding-Edge</a>, <a href="https://github.com/phpstan/phpstan-strict-rules#readme">Strict-Mode</a>, <a href="https://github.com/phpstan/phpstan-deprecation-rules#readme">Deprecation-Warnings</a>, <a href="https://github.com/phpstan/phpstan-phpunit#readme">PHPUnit</a>, <a href="https://staabm.github.io/archive.html#phpstan-dba">phpstan-dba</a>, <a href="https://tomasvotruba.com/blog/2018/05/21/is-your-code-readable-by-humans-cognitive-complexity-tells-you/">cognitive complexity</a>');
-$select = $field->getSelect();
-
-$select->addOption('Bleeding-Edge', realpath(__DIR__.'/../vendor/phpstan/phpstan/conf/bleedingEdge.neon'));
-$select->addOption('Strict-Mode', realpath(__DIR__.'/../vendor/phpstan/phpstan-strict-rules/rules.neon'));
-$select->addOption('Deprecation Warnings', realpath(__DIR__.'/../vendor/phpstan/phpstan-deprecation-rules/rules.neon'));
-$select->addOption('PHPUnit', realpath(__DIR__.'/../vendor/phpstan/phpstan-phpunit/rules.neon'));
-$select->addOption('phpstan-dba', realpath(__DIR__.'/../lib/phpstan-dba.neon'));
-$select->addOption('cognitive complexity', realpath(__DIR__.'/../lib/cognitive-complexity.neon'));
+/**
+ * Formularcode als Section via Fragment ausgeben.
+ */
 
 $fragment = new rex_fragment();
-$fragment->setVar('heading', '<i>Einstellungen werden <a href="'. rex_url::backendPage('rexstan/faq') .'">im FAQ erklärt</a></i>', false);
+$fragment->setVar('options', '<a class="btn btn-info" href="'. rex_url::backendPage('rexstan/faq') .'">Weitere Informationen in den FAQ</a>', false);
 $fragment->setVar('class', 'edit', false);
 $fragment->setVar('title', 'Settings', false);
 $fragment->setVar('body', $form->get(), false);
 echo $fragment->parse('core/page/section.php');
-
-$form_name = $form->getName();
-if (rex_post($form_name . '_save')) {
-    $postData = rex_post($form_name);
-    $addonPaths = $postData['addons'] ?? [];
-    $extensions = $postData['extensions'] ?? [];
-
-    $paths = [];
-    foreach ($addonPaths as $addonPath) {
-        $paths[] = $addonPath;
-    }
-
-    $includes = [];
-    foreach ($extensions as $extensionPath) {
-        $includes[] = $extensionPath;
-    }
-
-    RexStanUserConfig::save((int) $postData['level'], $paths, $includes);
-}
