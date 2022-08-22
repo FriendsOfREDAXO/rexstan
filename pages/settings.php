2ebb56133ac5a18d747f8706543971d159e6491d
Markus Staab <maggus.staab@googlemail.com>
2022-08-22 14:32:45 +0200

    simplify

--- pages/settings.php
+++ pages/settings.php
@@ -1,11 +1,4 @@
 <?php
-/**
- * Konfigurationsseite des Addons / Settings.
- *
- * Das Formular wird mit der Form-Klasse RexStanSettings, basierend auf rex_config_form, aufsetzt.
- * Die Parametriesierung und der Formularaufbaue (Felder) ist komplett in RexStanSettings.php zu finden
-*/
-
 $form = redaxo\phpstan\RexStanSettings::factory('rexstan');
 $faqUrl = rex_url::backendPage('rexstan/faq');
 
@@ -14,7 +7,7 @@ $faqUrl = rex_url::backendPage('rexstan/faq');
  */
 
 $fragment = new rex_fragment();
-$fragment->setVar('options', '<a class="btn btn-info" href="'. rex_url::backendPage('rexstan/faq') .'">Weitere Informationen in den FAQ</a>', false);
+$fragment->setVar('options', '<a class="btn btn-info" href="'. $faqUrl .'">Weitere Informationen in den FAQ</a>', false);
 $fragment->setVar('class', 'edit', false);
 $fragment->setVar('title', 'Settings', false);
 $fragment->setVar('body', $form->get(), false);
