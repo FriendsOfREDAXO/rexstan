c08b24bd515c869a67d681164c632c4655647312
christophboecker <christoph_boecker@t-online.de>
2022-09-19 16:42:19 +0200

    Load JS added

--- boot.php
+++ boot.php
@@ -5,6 +5,7 @@ $addon = rex_addon::get('rexstan');
 if (rex::isBackend() && is_object(rex::getUser()) && 'rexstan' === rex_be_controller::getCurrentPagePart(1)) {
     rex_view::addCssFile($addon->getAssetsUrl('rexstan.css'));
     rex_view::addJsFile($addon->getAssetsUrl('confetti.min.js'));
+    rex_view::addJsFile($addon->getAssetsUrl('rexstan.js'));
 }
 
 rex_extension::register('PACKAGE_CACHE_DELETED', static function (rex_extension_point $ep) {
