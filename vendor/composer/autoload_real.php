8fa83e1b2d530c02ff167c4b48857c72c148c76a
Markus Staab <maggus.staab@googlemail.com>
2023-02-11 11:11:51 +0100

    composer update vendor/

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -32,15 +32,15 @@ class ComposerAutoloaderInit9cf8af24a7a084f114b4553be2a1ff9f
         $loader->register(true);
 
         $filesToLoad = \Composer\Autoload\ComposerStaticInit9cf8af24a7a084f114b4553be2a1ff9f::$files;
-        $requireFile = static function ($fileIdentifier, $file) {
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
             if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
                 $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
 
                 require $file;
             }
-        };
+        }, null, null);
         foreach ($filesToLoad as $fileIdentifier => $file) {
-            ($requireFile)($fileIdentifier, $file);
+            $requireFile($fileIdentifier, $file);
         }
 
         return $loader;
