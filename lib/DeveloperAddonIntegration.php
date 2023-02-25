11a5f07ffb2e62f65f0e0fd2012c47290faf6890
Markus Staab <maggus.staab@googlemail.com>
2023-02-25 11:46:24 +0100

    extracted DeveloperAddonIntegration

---
+++ lib/DeveloperAddonIntegration.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace rexstan;
+
+final class DeveloperAddonIntegration {
+    static public function getModulesDir(): ?string {
+        if (class_exists(rex_developer_manager::class)) {
+            return rex_developer_manager::getBasePath() .'/modules/';
+        }
+
+        return null;
+    }
+
+    static public function getTemplatesDir(): ?string {
+        if (class_exists(rex_developer_manager::class)) {
+            return rex_developer_manager::getBasePath() .'/templates/';
+        }
+
+        return null;
+    }
+}
