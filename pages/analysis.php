7237df5cd92e9063a29ac5881dcd7aaa8835024c
Markus Staab <maggus.staab@googlemail.com>
2023-02-25 08:57:58 +0100

    warn when core was updated to rex 5.15 (#347)

--- pages/analysis.php
+++ pages/analysis.php
@@ -18,7 +18,13 @@ if (is_string($phpstanResult)) {
     // we moved settings files into config/.
     if (false !== stripos($phpstanResult, "neon' is missing or is not readable.")) {
         echo rex_view::warning(
-            "Das Einstellungsformat hat sich geändert. Bitte die <a href='". $settingsUrl ."'>Einstellungen öffnen</a> und erneut abspeichern. <br/><br/>".nl2br($phpstanResult)
+            "Das Einstellungsformat hat sich geändert. Bitte die <a href='".$settingsUrl."'>Einstellungen öffnen</a> und erneut abspeichern. <br/><br/>".nl2br(
+                $phpstanResult
+            )
+        );
+    } elseif (false !== stripos($phpstanResult, "polyfill-php8") && false !== stripos($phpstanResult, "does not exist")) {
+        echo rex_view::warning(
+            "Der REDAXO Core wurde aktualisiert. Bitte das rexstan AddOn re-installieren. <br/><br/>".nl2br($phpstanResult)
         );
     } else {
         echo rex_view::error(
