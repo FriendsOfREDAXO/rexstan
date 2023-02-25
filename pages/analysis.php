9af81fb831b012c539f73024a49659da414f7854
Markus Staab <maggus.staab@googlemail.com>
2023-02-25 11:07:55 +0100

    extract rexstan_level9_jseffect()

--- pages/analysis.php
+++ pages/analysis.php
@@ -97,29 +97,7 @@ if (
         echo rex_view::success('Gratulation, es wurden keine Fehler in Level '. $level .' gefunden.');
 
         if (9 === $level) {
-            echo '<script>
-                var duration = 10 * 1000;
-                var animationEnd = Date.now() + duration;
-                var defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
-
-                function randomInRange(min, max) {
-                  return Math.random() * (max - min) + min;
-                }
-
-                var interval = setInterval(function() {
-                  var timeLeft = animationEnd - Date.now();
-
-                  if (timeLeft <= 0) {
-                    return clearInterval(interval);
-                  }
-
-                  var particleCount = 50 * (timeLeft / duration);
-                  // since particles fall down, start a bit higher than random
-                  confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 } }));
-                  confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 } }));
-                }, 250);
-                </script>
-            ';
+            echo rexstan_level9_jseffect();
         } else {
             echo '<p>In den <a href="'. rex_url::backendPage('rexstan/settings') .'">Einstellungen</a>, solltest du jetzt das nächste Level anvisieren.</p>';
         }
@@ -138,6 +116,33 @@ if (
 
         echo rexstan_renderFileBlock($linkFile, $fileResult['messages']);
     }
+}
+
+function rexstan_level9_jseffect() {
+    return
+        '<script>
+        var duration = 10 * 1000;
+        var animationEnd = Date.now() + duration;
+        var defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
+
+        function randomInRange(min, max) {
+          return Math.random() * (max - min) + min;
+        }
+
+        var interval = setInterval(function() {
+          var timeLeft = animationEnd - Date.now();
+
+          if (timeLeft <= 0) {
+            return clearInterval(interval);
+          }
+
+          var particleCount = 50 * (timeLeft / duration);
+          // since particles fall down, start a bit higher than random
+          confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 } }));
+          confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 } }));
+        }, 250);
+        </script>
+    ';
 }
 
 /**
