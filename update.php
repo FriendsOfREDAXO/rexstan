ecc97e9b1d8ae1ebb20658fcac63eba02f0983f2
Markus Staab <maggus.staab@googlemail.com>
2023-03-18 13:38:10 +0100

    remove old config values on update (#412)

--- update.php
+++ update.php
@@ -3,3 +3,7 @@
 // trigger the installation procedures also on updates
 $addon = rex_addon::get('rexstan');
 $addon->includeFile(__DIR__ . '/install.php');
+
+// remove old config values
+$addon->removeConfig('includes');
+$addon->removeConfig('paths');
