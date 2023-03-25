c7282d8de53f9d6406d2483f9e2fa771dbb19e6b
tbaddade <thomas@redaxo.org>
2023-03-25 11:09:12 +0100

    Icon nach Installation korrekt anzeigen
    close #370

--- boot.php
+++ boot.php
@@ -8,7 +8,11 @@ if (rex::isBackend() && is_object(rex::getUser())) {
     rex_extension::register('OUTPUT_FILTER', function(\rex_extension_point $ep) use ($addon) {
         $svg = \rex_file::get($addon->getAssetsPath('rexstan_dino.svg'));
 
-        $ep->setSubject(str_replace('<i class="rexstan-navigation-icon"></i>', '<span class="rexstan-navigation-icon">'.$svg.'</span>', $ep->getSubject()));
+        $ep->setSubject(str_replace(
+            '<i class="rexstan-navigation-icon"></i>',
+            '<span class="rexstan-navigation-icon" style="display: inline-block; width: 20px; height: 20px; margin-left: -28px; margin-right: 3px; vertical-align: top;">'.$svg.'</span>',
+            $ep->getSubject()
+        ));
     });
 
     rex_view::addCssFile($addon->getAssetsUrl('rexstan.css'));
