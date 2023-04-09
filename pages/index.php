252c62dbc98f345a4ce111c18a93604e3c0dbd5a
Markus Staab <maggus.staab@googlemail.com>
2023-04-09 09:28:04 +0200

    use loader on slow pages (#476)

--- pages/index.php
+++ pages/index.php
@@ -4,6 +4,21 @@
 
 $logoPath = $this->getAssetsUrl('rexstan.svg');
 
+// in the future this should instead use the native redaxo core loader api
+// https://github.com/redaxo/redaxo/pull/5664
+
+echo '
+<script type="text/javascript">
+    document.addEventListener("DOMContentLoaded", function () {
+      document.querySelectorAll(".use-loader").forEach(function(el) {
+        el.addEventListener("click", function () {
+            document.querySelector("#rex-js-ajax-loader").classList.add("rex-visible");
+        });
+      });
+    }, false);
+</script>
+';
+
 echo rex_view::title('<span class="rexstan-logo"><img src="'.$logoPath.'" width="200" height="90" ></span>');
 
 rex_be_controller::includeCurrentPageSubPath();
