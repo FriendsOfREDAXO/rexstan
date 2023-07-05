d4e62e681b0edd2d0df7e9f652ba0bd9a7870888
tbaddade <thomas@redaxo.org>
2023-07-05 14:11:32 +0200

    nonce Attribute hinzugefügt

--- pages/summary.php
+++ pages/summary.php
@@ -12,7 +12,7 @@ if ($result === null) {
 
 ?>
 <div style="width: 80%; background-color: rgb(32, 43, 53)">
-    <?php echo rex_file::get(rex_addon::get('rexstan')->getDataPath().'/baseline-graph.html'); ?>
+    <?php echo str_replace('<script>', '<script nonce="'.rex_response::getNonce().'">', rex_file::get(rex_addon::get('rexstan')->getDataPath().'/baseline-graph.html')); ?>
 </div>
 <table class="table table-striped table-hover" style="width: auto;">
     <tbody>
