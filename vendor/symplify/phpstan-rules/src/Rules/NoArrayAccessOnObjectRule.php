5df5cc0763098d3a0c4c5c29232f1e7cc066b71c
Markus Staab <maggus.staab@googlemail.com>
2023-08-23 07:11:15 +0200

    update vendors

--- vendor/symplify/phpstan-rules/src/Rules/NoArrayAccessOnObjectRule.php
+++ vendor/symplify/phpstan-rules/src/Rules/NoArrayAccessOnObjectRule.php
@@ -8,7 +8,7 @@ use PhpParser\Node;
 use PhpParser\Node\Expr\ArrayDimFetch;
 use PHPStan\Analyser\Scope;
 use PHPStan\Rules\Rule;
-use Symplify\PHPStanRules\Matcher\ArrayStringAndFnMatcher;
+use PHPStan\Type\ObjectType;
 use Symplify\RuleDocGenerator\Contract\DocumentedRuleInterface;
 use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
 use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
@@ -19,24 +19,14 @@ use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
 final class NoArrayAccessOnObjectRule implements Rule
 {
     /**
-     * @readonly
-     * @var \Symplify\PHPStanRules\Matcher\ArrayStringAndFnMatcher
-     */
-    private $arrayStringAndFnMatcher;
-    /**
      * @var string
      */
     public const ERROR_MESSAGE = 'Use explicit methods over array access on object';
 
     /**
-     * @var array<class-string>
+     * @var string[]
      */
-    private const ALLOWED_CLASSES = ['SplFixedArray', 'SimpleXMLElement'];
-
-    public function __construct(ArrayStringAndFnMatcher $arrayStringAndFnMatcher)
-    {
-        $this->arrayStringAndFnMatcher = $arrayStringAndFnMatcher;
-    }
+    private const ALLOWED_CLASSES = ['SplFixedArray', 'SimpleXMLElement', 'Iterator', 'Aws\ResultInterface', 'Symfony\Component\Form\FormInterface'];
 
     /**
      * @return class-string<Node>
@@ -52,15 +42,13 @@ final class NoArrayAccessOnObjectRule implements Rule
      */
     public function processNode(Node $node, Scope $scope): array
     {
-        $varStaticType = $scope->getType($node->var);
-
-        $classNames = $varStaticType->getObjectClassNames();
-        if ($classNames === []) {
+        $varType = $scope->getType($node->var);
+        if (! $varType instanceof ObjectType) {
             return [];
         }
 
-        foreach ($classNames as $className) {
-            if ($this->arrayStringAndFnMatcher->isMatchWithIsA($className, self::ALLOWED_CLASSES)) {
+        foreach (self::ALLOWED_CLASSES as $allowedClass) {
+            if ($varType->isInstanceOf($allowedClass)->yes()) {
                 return [];
             }
         }
