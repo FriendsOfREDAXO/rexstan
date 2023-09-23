39d6a6b60dafb05c9b768623da4e2ad5ca942407
Markus Staab <maggus.staab@googlemail.com>
2023-09-23 12:00:10 +0200

    implement rexstan:analyze path arg and level option (#584)
    Co-authored-by: Marcel Kuhmann <marcel@dermarcel.eu>

--- lib/command.php
+++ lib/command.php
@@ -3,7 +3,9 @@
 namespace rexstan;
 
 use rex_console_command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class rexstan_command extends rex_console_command
@@ -16,13 +18,40 @@ class rexstan_command extends rex_console_command
         $this
             ->setName('rexstan:analyze')
             ->setDescription('Run static code analysis')
+            ->addArgument('path', InputArgument::OPTIONAL, 'File or directoy path to analyze')
+            ->addOption('level', 'l', InputOption::VALUE_REQUIRED, 'Rule level (0-9)')
         ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        echo RexStan::runFromCli($exitCode);
+        $io = $this->getStyle($input, $output);
 
+        $path = null;
+        $level = null;
+        if ($input->getArgument('path') !== null) {
+            $analyzePath = getcwd() .'/../'. $input->getArgument('path');
+            $path = realpath($analyzePath);
+
+            if ($path === false) {
+                throw new \Exception('Invalid path: '. $input->getArgument('path'));
+            }
+        }
+
+        if ($input->hasOption('level')) {
+            $level = $input->getOption('level');
+            if ($level !== null && !preg_match('/^[0-9]$/', $level)) {
+                throw new \Exception('Invalid level: '. $level);
+            }
+        }
+
+        $result = RexStan::runFromCli($exitCode, $path, $level, $errorOutput);
+
+        if ($result !== '') {
+            $io->write($result);
+        }
+
+        // pass PHPStan exit code 1:1
         return $exitCode;
     }
 }
