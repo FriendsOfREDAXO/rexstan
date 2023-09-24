397b664d41a163350c66384d90fb2f22b697dc17
Markus Staab <maggus.staab@googlemail.com>
2023-09-24 10:54:24 +0200

    Update RexPhpVersion.php

--- lib/RexPhpVersion.php
+++ lib/RexPhpVersion.php
@@ -5,10 +5,14 @@ namespace rexstan;
 final class RexPhpVersion
 {
     /**
+     * @param int|numeric-string $version1
      * @param int|numeric-string $version2
      */
-    public static function isEqualMajorMinor(int $version1, $version2): bool
+    public static function isEqualMajorMinor($version1, $version2): bool
     {
+        $version1 = (int) $version1;
+        $version2 = (int) $version2;
+
         $v1 = (int) ($version1 / 100);
         $v2 = (int) ($version2 / 100);
 
