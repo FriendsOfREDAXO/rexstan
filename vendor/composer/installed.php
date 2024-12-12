dd64511770e9eeaa7440a18d1106e08b6d48a8e1
Markus Staab <maggus.staab@googlemail.com>
2024-12-12 20:37:05 +0100

    Update installed.php

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '77c04be391846239eca56b785cf4ab1f2995bf25',
+        'reference' => '6fa77e0ebca8cadcfef7e37bac776e3ada9ed0a4',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '77c04be391846239eca56b785cf4ab1f2995bf25',
+            'reference' => '6fa77e0ebca8cadcfef7e37bac776e3ada9ed0a4',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
